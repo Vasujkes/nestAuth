e52d914c164687fae9fde6bfcc3415df07eff0dd
Василий <strayko@MacBook-Pro-Vasilij.local>
2020-12-27 00:46:40 +0300

    feature добавил jwt

--- src/ modules/auth/auth.module.ts
+++ src/ modules/auth/auth.module.ts
@@ -4,10 +4,25 @@ import { AuthService } from './service/auth.service'
 import { LocalStrategy } from './service/local.strategy'
 import { PassportModule } from '@nestjs/passport'
 import { AuthController } from './controller/auth.controller'
+import { JwtModule } from '@nestjs/jwt'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtStrategy } from './service/jwt.strategy'
 
 @Module({
   controllers: [AuthController],
-  imports: [AdminModule, PassportModule],
-  providers: [AuthService, LocalStrategy],
+  imports: [
+    AdminModule,
+    PassportModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        secret: configService.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: configService.get<string>('JWT_EXPIRES_IN') },
+      }),
+      inject: [ConfigService],
+    }),
+  ],
+  providers: [AuthService, LocalStrategy, JwtStrategy],
 })
 export class AuthModule {}
--- src/ modules/auth/controller/auth.controller.ts
+++ src/ modules/auth/controller/auth.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Request, Post, UseGuards } from '@nestjs/common'
+import { Controller, Request, Post, UseGuards, Get } from '@nestjs/common'
 import { AuthGuard } from '@nestjs/passport'
+import { AuthService } from '../service/auth.service'
+import { AdminRepository } from '../../admin/servise/admin.repository'
 
 @Controller('auth')
 export class AuthController {
+  constructor(
+    private authService: AuthService,
+    private adminRepository: AdminRepository,
+  ) {}
+
   @UseGuards(AuthGuard('local'))
   @Post('login')
   async login(@Request() req) {
-    console.log(req.admin, req.user)
+    return this.authService.login(req.user)
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Get('profile')
+  GetProfile(@Request() req) {
     return req.user
+  }
+
+  @UseGuards(AuthGuard('jwt'))
+  @Post('refresh')
+  async refresh(@Request() req) {
+    const admin = await this.adminRepository.find(req.user.id)
+    return this.authService.login(admin)
   }
 }
